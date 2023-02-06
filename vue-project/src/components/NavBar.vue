d700c0d4bc5ec9b9311724b712a93954166871da
Tim Viklund Fjesme <tim.viklund.fjesme@iths.se>
2023-02-06 14:46:25 +0100

    First commit

---
+++ vue-project/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <ul>
+    <li>
+      <RouterLink to="/">Hem</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/about">About</RouterLink>
+    </li>
+  </ul>
+  <RouterView />
+</template>
---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>Detta är about page</p>
+</template>
+
+<style></style>
