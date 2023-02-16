f23b565a0e083aa61cd65086e0fe8f326eb6fa16
Tim <112405922+vikluund@users.noreply.github.com>
2023-02-16 18:11:36 +0100

    6

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup>
 import HeroImage from '../components/HeroImage.vue'
+import TheCharacters from '../components/TheCharacters.vue'
 </script>
 
 <template>
   <HeroImage/>
+  <div class="container text-center">
+    <div class="row align-items-start">
+      <TheCharacters/>
+      <TheCharacters/>
+      <TheCharacters/>
+    </div>
+  </div>
+
+
 </template>
